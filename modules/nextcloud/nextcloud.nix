da379eaabe9db7786570e9cd84db2764fe116991
pim <pim@NixBTW>
2026-02-28 19:46:52 +0100

    Moved to local nextcloud service

--- modules/nextcloud/nextcloud.nix
+++ modules/nextcloud/nextcloud.nix
@@ -1,32 +1,10 @@
 {
-  virtualisation = {
-    oci-containers.backend = "docker";
-    docker = {
+  services = {
+    nextcloud = {
       enable = true;
-      autoPrune.enable = true;
-    };
-  };
-
-  virtualisation.oci-containers = {
-    containers.nextcloud-aio-mastercontainer = {
-      image = "ghcr.io/nextcloud-releases/all-in-one:latest";
-      autoStart = true;
-      extraOptions = [
-        "--init"
-        "--sig-proxy=false"
-      ];
-
-      ports = [
-        "80:80"
-        "8081:443"
-        "8080:8080"
-        "8443:8443"
-      ];
-
-      volumes = [
-        "nextcloud_aio_mastercontainer:/mnt/docker-aio-config"
-        "/var/run/docker.sock:/var/run/docker.sock:ro"
-      ];
+      appstoreEnable = true;
+      settings = {
+      };
     };
   };
 }
