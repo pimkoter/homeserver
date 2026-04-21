b655688505f2a33d04c97164b3b2f9f0c406c91a
pim <pim@NixBTW>
2026-04-21 13:13:21 +0200

    updated jellyfin container

--- modules/jellyfin/containers/jellyfin.nix
+++ modules/jellyfin/containers/jellyfin.nix
@@ -10,18 +10,17 @@
           "/logs/jellyfin:/log"
           "/cache/jellyfin:/cache"
           "/config/jellyfin:/config"
-          "/media/movies:/media/movies"
-          "/media/shows:/media/tv"
+          "/media/movies:/movies"
+          "/media/shows:/tv"
         ];
         ports = [
           "8096:8096/tcp"
-          "7359:7359/udp"
         ];
         user = "1000:1000";
         log-driver = "journald";
-        #        environment = {
-        #          JELLYFIN_LOG_DIR = "log";
-        #        };
+        environment = {
+          JELLYFIN_LOG_DIR = "log";
+        };
         extraOptions = [
           "--network-alias=jellyfin"
           "--network=jellyfin"
