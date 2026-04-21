6d2c322a9a57040bf6c7776711f1a265958e6ee5
pim <pim@NixBTW>
2026-04-21 12:06:32 +0200

    added jellyfin container

---
+++ modules/jellyfin/containers/jellyfin.nix
@@ -0,0 +1,30 @@
+{
+  # Containers
+  virtualisation = {
+    docker.networks.jellyfin = {};
+
+    oci-containers.containers = {
+      "jellyfin" = {
+        image = "jellyfin/jellyfin:10.11.8";
+        autoStart = true;
+
+        volumes = [
+          "/cache/jellyfin:/cache:rw"
+          "/config/jellyfin:/config:rw"
+          "/media/movies:/media/movies:ro"
+          "/media/shows:/media/tv:ro"
+        ];
+        ports = [
+          "8096:8096/tcp"
+          "7359:7359/udp"
+        ];
+        user = "1000:1000";
+        log-driver = "journald";
+        extraOptions = [
+          "--network-alias=jellyfin"
+          "--network=jellyfin"
+        ];
+      };
+    };
+  };
+}
