ec0dc8a5f576b2dea827c1276ad3a9167fa7b32f
pim <pim@NixBTW>
2026-05-14 11:04:22 +0200

    Added Caddy again

---
+++ modules/pihole/caddy.nix
@@ -0,0 +1,31 @@
+{
+  config,
+  lib,
+  hosts,
+  ...
+}: let
+  inherit (lib) mapAttrsToList mkMerge;
+
+  mkVirtualHosts = hostName: hostCfg:
+    lib.mapAttrs' (
+      serviceName: serviceCfg: {
+        name = "${serviceName}.home";
+
+        value = {
+          extraConfig = ''
+            reverse_proxy ${hostCfg.ip}:${toString serviceCfg.port}
+          '';
+        };
+      }
+    )
+    (hostCfg.services or {});
+
+  virtualHosts =
+    mkMerge (mapAttrsToList mkVirtualHosts hosts);
+in {
+  services.caddy = {
+    enable = true;
+
+    virtualHosts = virtualHosts;
+  };
+}
