eae894b26d63e850e680deebf82280e06e7998e2
pim <pim@NixBTW>
2026-05-17 21:00:21 +0200

    removed tls internal

--- modules/caddy/caddy.nix
+++ modules/caddy/caddy.nix
@@ -11,12 +11,10 @@
 
     virtualHosts = {
       "pihole.${hosts.domain}".extraConfig = ''
-        tls internal
         reverse_proxy http://${hosts.pihole.ip}:${hosts.pihole.services.pihole}
       '';
 
       "jellyfin.${hosts.domain}".extraConfig = ''
-        tls internal
         reverse_proxy http://${hosts.jellyfin.ip}:${hosts.jellyfin.services.jellyfin}
       '';
     };
@@ -25,5 +23,7 @@
 
   environment.systemPackages = with pkgs; [
     nssTools
+    caddy
+    p11-kit
   ];
 }
