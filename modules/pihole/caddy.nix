beaff4036940a34c7975f6cb3eb53496ccf4ee62
pim <pim@NixBTW>
2026-05-17 19:57:19 +0200

    Added jellyfin to caddy

--- modules/pihole/caddy.nix
+++ modules/pihole/caddy.nix
@@ -8,10 +8,17 @@
       auto_https off
     '';
 
-    virtualHosts."pihole.home".extraConfig = ''
-      tls internal
-      reverse_proxy http://${hosts.pihole.ip}:${hosts.pihole.services.pihole}
-    '';
+    virtualHosts = {
+      "pihole.home".extraConfig = ''
+        tls internal
+        reverse_proxy http://${hosts.pihole.ip}:${hosts.pihole.services.pihole}
+      '';
+
+      "jellyfin.home".extraConfig = ''
+        tls internal
+        reverse_proxy http://${hosts.jellyfin.ip}:${hosts.jellyfin.services.jellyfin}
+      '';
+    };
   };
   networking.firewall.allowedTCPPorts = [81 444];
 }
