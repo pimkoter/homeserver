64ed21388b56b12b6c90bc11b9f0647b6a07c009
pim <pim@NixBTW>
2026-05-17 19:47:24 +0200

    Fixed some port issues

--- modules/pihole/caddy.nix
+++ modules/pihole/caddy.nix
@@ -5,8 +5,11 @@
     httpsPort = 444;
 
     virtualHosts."pihole.home".extraConfig = ''
+      tls internal
+      auto_https off
+
       reverse_proxy http://${hosts.pihole.ip}:${hosts.pihole.services.pihole}
     '';
   };
-  networking.firewall.allowedTCPPorts = [80 443];
+  networking.firewall.allowedTCPPorts = [81 444];
 }
