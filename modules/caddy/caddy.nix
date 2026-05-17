77aef045e51cc22293deca51b187e36bf862cf0f
pim <pim@NixBTW>
2026-05-17 21:54:16 +0200

    Added back tls internal

--- modules/caddy/caddy.nix
+++ modules/caddy/caddy.nix
@@ -10,14 +10,17 @@
 
     virtualHosts = {
       "pihole.${hosts.domain}.arpa".extraConfig = ''
+        tls internal
         reverse_proxy http://${hosts.pihole.ip}:${hosts.pihole.services.pihole}
       '';
 
       "jellyfin.${hosts.domain}.arpa".extraConfig = ''
+        tls internal
         reverse_proxy http://${hosts.jellyfin.ip}:${hosts.jellyfin.services.jellyfin}
       '';
 
       "proxmox.${hosts.domain}.arpa".extraConfig = ''
+        tls internal
         reverse_proxy http://${hosts.proxmox.ip}:${hosts.proxmox.services.proxmox}
       '';
     };
