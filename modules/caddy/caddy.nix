86c9aa115fce1ee1615637a778a3c683473fb876
pim <pim@NixBTW>
2026-05-18 18:03:33 +0200

    Fixed syntax error

--- modules/caddy/caddy.nix
+++ modules/caddy/caddy.nix
@@ -24,7 +24,7 @@
         reverse_proxy http://${hosts.jellyfin.ip}:${hosts.jellyfin.services.bazarr}
       '';
 
-     "jellyseerr.${hosts.domain}".extraConfig = ''
+      "jellyseerr.${hosts.domain}".extraConfig = ''
         tls internal
         reverse_proxy http://${hosts.jellyfin.ip}:${hosts.jellyfin.services.jellyseerr}
       '';
@@ -33,32 +33,29 @@
         tls internal
         reverse_proxy http://${hosts.jellyfin.ip}:${hosts.jellyfin.services.qbittorrent}
       '';
-   
-     "radarr.${hosts.domain}".extraConfig = ''
+
+      "radarr.${hosts.domain}".extraConfig = ''
         tls internal
         reverse_proxy http://${hosts.jellyfin.ip}:${hosts.jellyfin.services.radarr}
       '';
-  
-     "sonarr.${hosts.domain}".extraConfig = ''
+
+      "sonarr.${hosts.domain}".extraConfig = ''
         tls internal
         reverse_proxy http://${hosts.jellyfin.ip}:${hosts.jellyfin.services.sonarr}
       '';
-  
+
       "prowlarr.${hosts.domain}".extraConfig = ''
         tls internal
         reverse_proxy http://${hosts.jellyfin.ip}:${hosts.jellyfin.services.prowlarr}
       '';
 
-
-
-      proxmox.${hosts.domain}".extraConfig = ''
+      "proxmox.${hosts.domain}".extraConfig = ''
         tls internal
         reverse_proxy https://${hosts.proxmox.ip}:${hosts.proxmox.services.proxmox} {
           transport http {
             tls_insecure_skip_verify
           }
         }
-
       '';
     };
   };
